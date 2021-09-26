0fea923968c134aaf6c03110a9f673923a7511ee
EC2 Default User <ec2-user@ip-172-31-9-250.ap-northeast-1.compute.internal>
2021-09-26 16:08:50 +0000

    4-commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: session[:user_id])
   end
   
+  before_action :set_search
+
+  def set_search
+    @search = Room.ransack(params[:q])
+    @search_rooms = @search.result
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_20_024731) do
+ActiveRecord::Schema.define(version: 2021_09_26_072308) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "room_name"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2021_09_20_024731) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.string "password"
   end
 
 end
